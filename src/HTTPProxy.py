da5d7fdb77ae5ed471dac9e531a6381643114258
tyler@mbp <ouyang@cs.ucsb.edu>
2016-02-27 19:10:03 -0800

    chunked fix

--- src/HTTPProxy.py
+++ src/HTTPProxy.py
@@ -1,10 +1,14 @@
 import http.client
 import sys
+import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
 from urllib.parse import urlunsplit, urlsplit
 
+# import MITMResponseClass
+
 connTimeout = 9999
+# http.client.HTTPConnection.response_class = MITMResponseClass.MITMResponseClass
 
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
@@ -28,6 +32,7 @@ def run(server_class=ThreadedHTTPServer, handler_class=BaseHTTPRequestHandler, p
 class MITMHTTPRequestHandler(BaseHTTPRequestHandler):
     protocol_version = 'HTTP/1.1'
     establishedConns = {}
+    establishedConnsLock = threading.Lock()
 
     def do_GET(self):
         """Serve a GET request."""
@@ -37,67 +42,61 @@ class MITMHTTPRequestHandler(BaseHTTPRequestHandler):
         scheme = parsedURL.scheme
         host = parsedURL.netloc
         url = urlunsplit(('', '', parsedURL.path, parsedURL.query, ''))
-        print(host, url)
 
         contentLength = req.headers['Content-Length']
         reqBody = req.rfile.read(contentLength) if contentLength else None
 
         target = (scheme, host)
+        self.establishedConnsLock.acquire()
         if target not in self.establishedConns:
-            conn = self.buildConn(target)
+            conn, connLock = self.buildConn(target)
         else:
-            conn = self.establishedConns[target]
+            conn, connLock = self.establishedConns[target]
+        self.establishedConnsLock.release()
 
         try:
-            conn.request(self.command, url, reqBody, req.headers)
-        except http.client.CannotSendRequest:
-            conn = self.rebuildConn(target)
-            conn.request(self.command, url, reqBody, req.headers)
+            tries = 1
+            connLock.acquire()
+            print(host, url)
+            while tries <= 5:
+                try:
+                    conn.request(self.command, url, reqBody, req.headers)
+                except http.client.ImproperConnectionState:
+                    conn.connect()
+                try:
+                    res = conn.getresponse()
+                    self.respondToRequest(res)
+                except http.client.HTTPException:
+                    if tries == 5:
+                        req.send_error(404)
+                    print('tries'+str(tries))
+                    tries += 1
+                    continue
+                break
+        finally:
+            connLock.release()
 
-        res = conn.getresponse()
-        self.respondToRequest(res, self.readFrom(res))
-
-    def respondToRequest(self, res, resBody):
+    def respondToRequest(self, res):
         req = self
+        resBody = res.read()
         req.send_response_only(res.status)
         for header in res.getheaders():
-            req.send_header(header[0], header[1])
+            if header[0] == 'Transfer-Encoding':
+                req.send_header('Content-Length', len(resBody))
+            else:
+                req.send_header(header[0], header[1])
         req.end_headers()
         if self.command is not 'HEAD':
             req.wfile.write(resBody)
             req.wfile.flush()
 
-    def readFrom(self, res):
-        if res.fp is None:
-            return b""
-
-        if res.length is None:
-            s = res.fp.read()
-        else:
-            try:
-                s = res._safe_read(res.length)
-            except http.IncompleteRead:
-                res._close_conn()
-                raise
-            res.length = 0
-        res._close_conn()        # we read everything
-        return s
-
     do_HAED = do_GET
-
-    def do_POST(self):
-        req = self
-        contentLength = req.getheader('Content-Length')
-        req_body = req.rfile.read(contentLength) if contentLength else None
+    do_POST = do_GET
 
     def buildConn(self, target):
         global connTimeout
         conn = http.client.HTTPConnection(target[1], timeout=connTimeout)
-        self.establishedConns[target] = conn
-        return conn
+        connLock = threading.Lock()
+        self.establishedConns[target] = (conn, connLock)
+        return conn, connLock
 
-    def rebuildConn(self, target):
-        global connTimeout
-        conn = http.client.HTTPConnection(target[1], timeout=connTimeout)
-        self.establishedConns[target] = conn
-        return conn
