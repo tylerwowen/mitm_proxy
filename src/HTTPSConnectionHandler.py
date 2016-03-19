b1992b6ed69a0f6413a27a791e350a218ff79c9a
tyler@mbp <ouyang@cs.ucsb.edu>
2016-03-18 22:43:11 -0700

    switched to openssl

--- src/HTTPSConnectionHandler.py
+++ src/HTTPSConnectionHandler.py
@@ -1,8 +1,11 @@
 import os
-import pprint
 import socket
 import ssl
 import subprocess
+
+from OpenSSL import crypto
+from OpenSSL.crypto import FILETYPE_PEM, load_privatekey, load_certificate
+from OpenSSL.SSL import Context, SSLv23_METHOD, Connection
 
 CA_ROOT = '../certificates/'
 CERT_ROOT = '../temp_certs/'
@@ -51,33 +54,75 @@ OU = MyDivision
 
 
 def build_ssl_conns(client_socket, path):
-    server_ssl_sock = build_server_conn(path)
-    client_socket.sendall(('HTTP/1.1 %d %s\r\n\r\n' % (200, 'OK')).encode('latin-1', 'strict'))
-
-    cert_dict = server_ssl_sock.getpeercert()
-    crt_dir = generate_fake_cert(cert_dict)
-    client_ssl_sock = build_client_conn(client_socket, crt_dir)
-
-    return server_ssl_sock, client_ssl_sock
-
-
-def build_server_conn(path):
-    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    context.verify_mode = ssl.CERT_OPTIONAL
-    # context.load_verify_locations('/etc/ssl/certs/ca-bundle.crt')
-    context.set_default_verify_paths()
     host, port = parse_connect_path(path)
+    mitm = ManInTheMiddle(client_socket, host, port)
+    mitm.start_hacking()
 
-    ssl_sock = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=host)
-    ssl_sock.connect((host, port))
-    return ssl_sock
+    return mitm.server_ssl_sock, mitm.client_ssl_sock
 
 
-def build_client_conn(sock, crt_dir):
-    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    context.load_cert_chain(certfile=crt_dir, keyfile=GEN_KEY_DIR)
-    client_ssl_sock = context.wrap_socket(sock, server_side=True)
-    return client_ssl_sock
+class ManInTheMiddle:
+
+    def __init__(self, client_socket, host, port):
+        self.client_socket = client_socket
+        self.host = host
+        self.port = port
+        self.sni = None
+        self.client_ssl_sock = None
+        self.server_ssl_sock = None
+
+    def start_hacking(self):
+        self.client_socket.sendall(('HTTP/1.1 %d %s\r\n\r\n' % (200, 'OK')).encode('latin-1', 'strict'))
+        self.accept_client_conn()
+
+    def accept_client_conn(self):
+        context = Context(SSLv23_METHOD)
+        context.set_tlsext_servername_callback(self.prepare_handshake)
+
+        self.client_ssl_sock = Connection(context, self.client_socket)
+        self.client_ssl_sock.set_accept_state()
+        self.client_ssl_sock.do_handshake()
+
+    def prepare_handshake(self, connection):
+        raw_sni = connection.get_servername()
+        if raw_sni is not None:
+            self.sni = str(raw_sni, 'ascii')
+
+        self.build_server_conn()
+        cert_dict = self.server_ssl_sock.getpeercert()
+        crt_dir = generate_fake_cert(cert_dict)
+        try:
+            key, cert = load(crt_dir)
+        except crypto.Error:
+            raise CertificateRaceCondition
+        new_context = Context(SSLv23_METHOD)
+        new_context.use_privatekey(key)
+        new_context.use_certificate(cert)
+        connection.set_context(new_context)
+
+    def build_server_conn(self):
+        server_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        server_context.verify_mode = ssl.CERT_OPTIONAL
+        # context.load_verify_locations('/etc/ssl/certs/ca-bundle.crt')
+        server_context.set_default_verify_paths()
+        ssl_sock = server_context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=self.sni)
+        try:
+            ssl_sock.connect((self.host, self.port))
+        except socket.gaierror:
+            print(self.host, self.port)
+            raise ServerConnectionError
+        self.server_ssl_sock = ssl_sock
+
+
+def load(crt_dir):
+    crt = open(crt_dir)
+    key = open(GEN_KEY_DIR)
+    result = (
+        load_privatekey(FILETYPE_PEM, key.read()),
+        load_certificate(FILETYPE_PEM, crt.read()))
+    crt.close()
+    key.close()
+    return result
 
 
 def parse_connect_path(path):
@@ -117,9 +162,9 @@ def generate_csr(cert_dict):
 
 def generate_config(cert_dict):
     common_name = ''
-    for tuple in cert_dict['subject']:
-        if tuple[0][0] == 'commonName':
-            common_name = tuple[0][1]
+    for pairs in cert_dict['subject']:
+        if pairs[0][0] == 'commonName':
+            common_name = pairs[0][1]
 
     f = open(CERT_ROOT+cert_dict['serialNumber']+'.cfg', 'w')
     alt_names = cert_dict['subjectAltName']
@@ -144,15 +189,14 @@ def save_cert(cert_raw, name):
     return CERT_ROOT+name
 
 
-def deal_with_client(connstream):
-    data = connstream.recv(1024)
-    # empty data means the client is finished with us
-    while data:
-        data = connstream.recv(1024)
-        pprint.pprint(data)
-    # finished with client
+class CertificateRaceCondition(Exception):
+    pass
 
-if __name__ == "__main__":
+
+class ServerConnectionError(Exception):
+    pass
+
+# if __name__ == "__main__":
     # path = 'www.google.com:443'
     # server_ssl_sock = build_server_conn(path)
     # pprint.pprint(server_ssl_sock.getpeercert())
@@ -175,7 +219,5 @@ if __name__ == "__main__":
     #         # connstream.shutdown(socket.SHUT_RD)
     #         connstream.close()
     # build_ssl_conns(socket.socket(), 'google.com:443')
-    ssl_sock = build_server_conn('google.com:443')
-    cert_raw = ssl_sock.getpeercert(True)
-    save_cert(cert_raw, 'google_orig.crt')
+
 
